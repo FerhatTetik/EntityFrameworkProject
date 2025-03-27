eee72a76091635c5e8d0c8eff2836c88363b8ac1
FerhatTetik <ferhat.tetik.332@gmail.com>
2025-03-27 22:12:40 +0300

    Ignored .vs folder

---
+++ Egitim301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim301.EntityLayer.Concrete
+{
+     public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+
+
+
+    }
+}
